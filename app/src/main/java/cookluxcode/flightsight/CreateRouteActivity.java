15768ff2317728d4f0a8b59f99a98c5adf2cff13
Sept_en <gammerxpower@gmail.com>
2017-04-29 23:43:49 +0300

    Added DrawerLayout to CreateRoute activity

--- app/src/main/java/cookluxcode/flightsight/CreateRouteActivity.java
+++ app/src/main/java/cookluxcode/flightsight/CreateRouteActivity.java
@@ -7,10 +7,14 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Toast;
+import android.widget.ListView;
+import android.support.v4.widget.DrawerLayout;
 
 import com.google.android.gms.maps.model.LatLng;
 import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
@@ -32,11 +36,15 @@ public class CreateRouteActivity extends Activity {
     private static final String TAG = CreateRouteActivity.class.getSimpleName();
     private static final int REQUEST_CODE_FROM_LOCATION = 1;
     private static final int REQUEST_CODE_TO_LOCATION = 2;
+    private static final String[] menuTitles = { "New route", "Trips", "Settings" };
+
 
     @BindView(R.id.from_text) EditText fromText;
     @BindView(R.id.to_text) EditText toText;
     @BindView(R.id.create_button) Button createButton;
     @BindView(R.id.progress_bar) ProgressBar progressBar;
+    @BindView(R.id.drawer_layout) DrawerLayout drawerLayout;
+    @BindView(R.id.menu_list_view) ListView menuListView;
 
     LatLng from;
     LatLng to;
@@ -57,6 +65,10 @@ public class CreateRouteActivity extends Activity {
                 .build();
         serverApi = retrofit.create(ServerApi.class);
         requestId = new Random().nextInt();
+
+        // Drawer layout
+        menuListView.setAdapter(new ArrayAdapter<String>(this, R.layout.item_drawer_layout, menuTitles));
+        menuListView.setOnItemClickListener(new DrawerItemClickListener());
     }
 
     @OnClick(R.id.pick_from_button)
@@ -126,6 +138,18 @@ public class CreateRouteActivity extends Activity {
                 }
                 break;
 
+        }
+    }
+
+    private void selectMenuItem(int position) {
+
+    }
+
+
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            selectMenuItem (position);
         }
     }
 }
